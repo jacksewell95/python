710da13512c8fd1ece370ec2807c1a4a0bde5f5b
Jack <86314751+jacksewell95@users.noreply.github.com>
2022-04-15 15:54:47 +0100

    Colour and size label parameters

--- Tkinter.py
+++ Tkinter.py
@@ -4,7 +4,12 @@ import tkinter.ttk as ttk
 # Create a new window using tk.Tk() and assign to 'window'
 window = tk.Tk()
 # Add a widget - use tk.Label() to add text
-message = tk.Label(text="Python is cool! :D")
+# Width and height are measured in text units, so will naturally be taller than it is wide 
+message = tk.Label(text="Python is cool! :D",
+                   fg="#00ff00",
+                   bg="black",
+                   width=20,
+                   height=10)
 # Add the Label widget to the window using its .pack() method
 message.pack()
 # Tkinter sizes the window as small as it can
@@ -14,6 +19,8 @@ window.mainloop()
 
 # Repeat using the themed widgets ttk.Label()
 new_window = tk.Tk()
-new_message = ttk.Label(text="Themed widgets are cooler!! :D")
+new_message = ttk.Label(text="Themed widgets aren't so good. The new parameters don't work :'(",
+                        foreground="white",
+                        background="black",)
 new_message.pack()
 new_window.mainloop()
